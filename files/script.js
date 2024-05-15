fbbb8bae8d164b8ee5dba86fcda68d067c342918
NetMachine <felipeabarcia@gmail.com>
2024-05-15 16:47:35 -0400

    zbznz

---
+++ files/script.js
@@ -0,0 +1,29 @@
+// Reemplaza 'username' y 'repositorio' con tus propios datos
+const repoUrl = 'https://api.github.com/repos/NetMachine/iMONKEy/contents/download';
+const fileList = document.querySelector('.file-list');
+
+fetch(repoUrl)
+  .then(response => response.json())
+  .then(files => {
+    files.forEach(file => {
+      if (!file.type || file.type === 'file') {
+        const listItem = document.createElement('li');
+        const linkWrapper = document.createElement('div');
+        const link = document.createElement('a');
+        link.href = file.download_url;
+        link.textContent = file.name;
+        const downloadBtn = document.createElement('button');
+        downloadBtn.classList.add('download-btn');
+        downloadBtn.textContent = 'Descargar';
+        downloadBtn.onclick = () => window.location.href = file.download_url;
+        linkWrapper.appendChild(link);
+        listItem.appendChild(linkWrapper);
+        listItem.appendChild(downloadBtn);
+        fileList.appendChild(listItem);
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error al obtener los archivos:', error);
+    fileList.innerHTML = '<li>No se pudieron cargar los archivos.</li>';
+  });
