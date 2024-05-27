3f19eed533305940c8df80f68ca0ff6b3d425885
NetMachine <felipeabarcia@gmail.com>
2024-05-26 21:26:32 -0400

    gh

---
+++ reparacion-iphone/script.js
@@ -0,0 +1,20 @@
+const mainNav = document.getElementById('main-nav');
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+    navToggle.classList.toggle('open');
+});
+
+document.addEventListener('click', (event) => {
+    if (!mainNav.contains(event.target) && event.target !== navToggle) {
+        mainNav.classList.remove('open');
+        navToggle.classList.remove('open');
+    }
+});
+
+document.addEventListener('scroll', () => {
+    mainNav.classList.remove('open');
+    navToggle.classList.remove('open');
+});
